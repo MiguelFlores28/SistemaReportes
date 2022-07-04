a1ecd863718d28ee5889b09c3fa81a619663a7c3
MiguelFlores28 <supermafl18@gmail.com>
2022-07-03 20:54:24 -0500

    Matrimonios y Resultados terminados y mejorados

--- router/router.tablas.js
+++ router/router.tablas.js
@@ -24,7 +24,16 @@ router.get('/defunciones', async(req, res, next) => {
 });
 
 router.get('/matrimonios', async(req, res, next) => {
-    res.render('./tablas/matrimonios', {titulopag: "Matrimonios"});
+    var sql = "SELECT * FROM matrimonios; SELECT SUM(total) AS Tot FROM matrimonios";
+    conn.query(sql, [1, 2], (err,data, fields)=>{
+        if (err) {throw err}
+        else {
+            console.log("Resultado: "+ JSON.stringify(data[0]));
+            console.log("//////////////////////////////////////////////////////////////////////////////");
+            console.log("Resultado: " + JSON.stringify(data[1]));
+            res.render('./tablas/matrimonios', {titulopag: "Matrimonios", datosConsulta1:data[0], datosConsulta2:data[1]});      
+        }
+    })
 });
 
 
@@ -42,7 +51,7 @@ router.post('/resultados', async(req, res, next) => {
         case "defunciones":
             if(body.siglo == "ambos"){
                 sql+="(siglo= 'XVII' OR siglo= 'XVIII') AND ";
-                if(((body.anio1 >= 1601 && body.anio1 <= 1699)||(body.anio1 >= 1700 && body.anio1 <= 1799)) && ((body.anio2 >= 1601 && body.anio2 <= 1699)||(body.anio2 >= 1700 && body.anio2 <= 1799))){
+                if(((body.anio1 >= 1601 && body.anio1 <= 1699)||(body.anio1 >= 1700 && body.anio1 <= 1799)) && ((body.anio2 >= body.anio1 && body.anio2 <= 1699)||(body.anio2 >= body.anio1 && body.anio2 <= 1799))){
                     sql+="(anio>="+body.anio1+" AND anio<="+body.anio2+") AND ";
                 }
                 else{
@@ -52,17 +61,23 @@ router.post('/resultados', async(req, res, next) => {
             }
             else if(body.siglo == 'XVII'){
                 sql+="(siglo= 'XVII') AND ";
-                if((body.anio1 >= 1601 && body.anio1 <= 1699) && (!body.anio2)||(body.anio1 >= 1601 && body.anio1 <= 1699) && (body.anio2 >= 1601 && body.anio2 <= 1699)){
+                if((body.anio1 >= 1601 && body.anio1 <= 1699) && (!body.anio2)){
                     sql+="(anio="+body.anio1+") AND ";
                 }
+                else if((body.anio1 >= 1601 && body.anio1 <= 1699) && (body.anio2 >= body.anio1 && body.anio2 <= 1699)){
+                    sql+="(anio>="+body.anio1+" AND anio<="+body.anio2+") AND ";
+                }    
                 else{
                     msg="Valores ingresados fuera del rango especificado. Favor de regresar y verificar que se encuentren en el rango de 1601 a 1699";
                 }
             }
             else{
                 sql+="(siglo= 'XVIII') AND ";
-                if ((body.anio2 >= 1700 && body.anio2 <= 1799) && (!body.anio1)||(body.anio2 >= 1700 && body.anio2 <= 1799) && (body.anio1 >= 1700 && body.anio1 <= 1799)){
+                if ((body.anio2 >= 1700 && body.anio2 <= 1799) && (!body.anio1)){
                     sql+="(anio >="+body.anio2+") AND ";
+                }
+                else if ((body.anio2 >= 1700 && body.anio2 <= 1799) && (body.anio1 >= body.anio1 && body.anio1 <= 1799)){
+                    sql+="(anio>="+body.anio1+" AND anio<="+body.anio2+") AND ";
                 }
                 else{
                     msg="Valores ingresados fuera del rango especificado. Favor de regresar y verificar que se encuentren en el rango de 1700 a 1799";
@@ -93,7 +108,54 @@ router.post('/resultados', async(req, res, next) => {
                 sql+="(genero='M');";
             }
              break;
-        case "matrimonios": break;
+
+        case "matrimonios":
+            if(body.siglo == "ambos"){
+                sql+="(siglo= 'XVII' OR siglo= 'XVIII') AND ";
+                if(((body.anio1 >= 1601 && body.anio1 <= 1699)||(body.anio1 >= 1700 && body.anio1 <= 1799)) && ((body.anio2 >= body.anio1 && body.anio2 <= 1699)||(body.anio2 >= body.anio1 && body.anio2 <= 1799))){
+                    sql+="(anio>="+body.anio1+" AND anio<="+body.anio2+") AND ";
+                }
+                else{
+                    msg="Valores ingresados fuera del rango especificado. Favor de regresar y verificar que se encuentren en el rango de 1601 a 1799";
+
+                }
+            }
+            else if(body.siglo == 'XVII'){
+                sql+="(siglo= 'XVII') AND ";
+                if((body.anio1 >= 1601 && body.anio1 <= 1699) && (!body.anio2)){
+                    sql+="(anio="+body.anio1+") AND ";
+                }
+                else if((body.anio1 >= 1601 && body.anio1 <= 1699) && (body.anio2 >= body.anio1 && body.anio2 <= 1699)){
+                    sql+="(anio>="+body.anio1+" AND anio<="+body.anio2+") AND ";
+                }    
+                else{
+                    msg="Valores ingresados fuera del rango especificado. Favor de regresar y verificar que se encuentren en el rango de 1601 a 1699";
+                }
+            }
+            else{
+                sql+="(siglo= 'XVIII') AND ";
+                if ((body.anio2 >= 1700 && body.anio2 <= 1799) && (!body.anio1)){
+                    sql+="(anio >="+body.anio2+") AND ";
+                }
+                else if ((body.anio2 >= 1700 && body.anio2 <= 1799) && (body.anio1 >= body.anio1 && body.anio1 <= 1799)){
+                    sql+="(anio>="+body.anio1+" AND anio<="+body.anio2+") AND ";
+                }
+                else{
+                    msg="Valores ingresados fuera del rango especificado. Favor de regresar y verificar que se encuentren en el rango de 1700 a 1799";
+                }
+            }
+
+            if(body.genero_f=="Todas"){
+                sql+="(etnia_f= 'CASTIZA' OR etnia_f= 'ESPAÑOLA' OR etnia_f= 'INDIA' OR etnia_f='MESTIZA' OR etnia_f='MORISCA' OR etnia_f='MULATA' OR etnia_f='FEMENINA SIN CATEG.' OR etnia_f='NEGRA') AND";
+            }else{
+                sql+="(etnia_f= '"+body.genero_f+"') AND";
+            }
+            if(body.genero_m=="Todas"){
+                sql+="(etnia_m= 'CASTIZO' OR etnia_m= 'ESPAÑOL' OR etnia_m= 'INDIO' OR etnia_m='MESTIZO' OR etnia_m='MORISCO' OR etnia_m='MULATO' OR etnia_m='MASCULINO SIN CATEG.' OR etnia_m='NEGRO')";
+            }else{
+                sql+="(etnia_m= '"+body.genero_m+"')";
+            }
+            break;
     }
     console.log("SQL="+sql);
     console.log(msg);
@@ -101,7 +163,7 @@ router.post('/resultados', async(req, res, next) => {
         if(err) throw err;
         else{
             console.log("Resultado "+ JSON.stringify(data));
-            res.render('./resultados/res', {Mensaje:msg, titulopag: "Resultados", datosConsulta:data});
+            res.render('./resultados/res', {Mensaje:msg, titulopag: "Resultados", datosConsulta:data, NT:body.nombre_tabla});
         }
     });
 });
